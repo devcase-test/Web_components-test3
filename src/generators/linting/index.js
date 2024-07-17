96facdf20e391f863182dae3fa25123b68facee6
Sytac Dev <31990977+sytac-bot@users.noreply.github.com>
2024-07-17 10:24:32 +0200

    Initial devcase commit

---
+++ src/generators/linting/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+import { LintingEsLintMixin } from '../linting-eslint/index.js';
+import { LintingPrettierMixin } from '../linting-prettier/index.js';
+
+export const LintingMixin = subclass =>
+  class extends LintingPrettierMixin(LintingEsLintMixin(subclass)) {
+    async execute() {
+      await super.execute();
+
+      // extend package.json
+      this.copyTemplateJsonInto(
+        `${__dirname}/templates/package.json`,
+        this.destinationPath('package.json'),
+      );
+
+      await this.copyTemplates(`${__dirname}/templates/static/**/*`);
+    }
+  };
